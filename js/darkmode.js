6dd9bbe2c1458827feff47c9fb21612b89dac441
hridaya <kawaaii@nocturn9x.space>
2023-08-25 10:06:13 +0545

    Make use of localStorage for uniform use of DarkMode
    - ChatGPT'ed

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,4 +1,25 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+// Function to set the dark mode styling
+function setDarkMode(darkMode) {
+  var element = document.body;
+
+  if (darkMode) {
+    element.classList.add("dark-mode");
+  } else {
+    element.classList.remove("dark-mode");
   }
+}
+
+// Function to toggle dark mode and update localStorage
+function toggleDarkMode() {
+  var currentDarkMode = localStorage.getItem("darkMode") === "true";
+  var newDarkMode = !currentDarkMode;
+
+  localStorage.setItem("darkMode", newDarkMode);
+  setDarkMode(newDarkMode);
+}
+
+// Apply dark mode setting on page load
+document.addEventListener("DOMContentLoaded", function () {
+  var currentDarkMode = localStorage.getItem("darkMode") === "true";
+  setDarkMode(currentDarkMode);
+});
