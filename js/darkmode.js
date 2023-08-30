1e1eefccba8a9c3b5caee25c84a3cbac0ca42de9
sagarcrestha81 <sagarcrestha81@gmail.com>
2023-08-30 22:40:30 +0545

    js: Fixup darkMode logic

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,28 +1,16 @@
-function setDarkMode(darkMode) {
+function setDarkMode() {
   var element = document.body;
-
-  if (darkMode) {
+  if (localStorage.getItem("darkMode") === "true") {
     element.classList.add("dark-mode");
   } else {
     element.classList.remove("dark-mode");
   }
 }
 
+setDarkMode();
+
 function toggleDarkMode() {
-  var currentDarkMode = localStorage.getItem("darkMode") === "true";
-  var newDarkMode = !currentDarkMode;
-
-  localStorage.setItem("darkMode", newDarkMode);
-  setDarkMode(newDarkMode);
+  mode = localStorage.getItem("darkMode") === "true" ? "false" : "true";
+  localStorage.setItem("darkMode", mode);
+  setDarkMode();
 }
-
-// Initialize dark mode setting from localStorage or set it to false by default
-document.addEventListener("DOMContentLoaded", function () {
-  var currentDarkMode = localStorage.getItem("darkMode") === "true";
-  setDarkMode(currentDarkMode);
-});
-
-// Set dark mode to false by default when the tab is closed
-window.addEventListener("beforeunload", function () {
-  localStorage.setItem("darkMode", false);
-});
